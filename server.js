5686b5b1b4c10b1686633a182786d87487c1dd54
Alexandre Barbier <barbier.a@sfeir.com>
2015-11-20 23:28:29 +0100

    add base project config

---
+++ server.js
@@ -0,0 +1,8 @@
+const apiKey = require('./api-key');
+
+const request = require('request');
+request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/Elwanna?api_key=' + apiKey , function (error, response) {
+  if (!error && response.statusCode == 200) {
+    console.log(response);
+  }
+})
