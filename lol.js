642c169497fcdb6e270ce51641cdf712017b208c
tonygoussot <tonygoussot@users.noreply.github.com>
2015-11-21 00:50:56 +0100

    feat(lol): getMatches

--- lol.js
+++ lol.js
@@ -2,12 +2,20 @@ const request = require('request');
 const apiKey  = require('./api-key');
 
 module.exports = {
-  getSummonerByName: function(summonerName, callback) {
-    console.log(summonerName);
-    request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey, function(error, response) {
-        if (!error && response.statusCode == 200) {
-            callback(response.body);
-        }
-    });
-  }
+    getSummonerByName : function(summonerName, callback) {
+        console.log(summonerName);
+        request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey, function(error, response) {
+            if (!error && response.statusCode == 200) {
+                callback(response.body);
+            }
+        });
+    },
+    getRankedMatches  : function(summonerId, callback) {
+        console.log(summonerId);
+        request('https://euw.api.pvp.net/api/lol/euw/v2.2/matchlist/by-summoner/' + summonerId + '?rankedQueues=RANKED_SOLO_5x5&seasons=PRESEASON2016&api_key=' + apiKey, function(error, response) {
+            if (!error && response.statusCode == 200) {
+                callback(response.body);
+            }
+        });
+    }
 };
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const lol = require('./lol');
+const lol     = require('./lol');
 
 const app = express();
 
-app.get('/summoner/:summonerName', function (req, res) {
-  lol.getSummonerByName(req.params.summonerName, function(lolres){
-    res.send(lolres);
+app.get('/summoner/:summonerName', function(req, res) {
+  lol.getSummonerByName(req.params.summonerName, function(summonerData) {
+    res.send(summonerData);
   });
 });
 
-const server = app.listen(3000, function () {
+app.get('/matches/:summonerId', function(req, res) {
+  lol.getRankedMatches(req.params.summonerId, function(matches) {
+    res.send(matches);
+  });
+});
+
+const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
 
