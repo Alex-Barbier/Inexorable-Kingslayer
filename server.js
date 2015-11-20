b2a6b9f52e88bd33a55c05d12d187e1965046f3d
Alexandre Barbier <barbier.a@sfeir.com>
2015-11-21 00:18:16 +0100

    feat(server): add new route getting summoner info

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const apiKey  = require('./api-key');
 const express = require('express');
 const request = require('request');
 
-request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/Elwanna?api_key=' + apiKey, function(error, response) {
-    if (!error && response.statusCode == 200) {
-        console.log(response.body);
-    }
+const app = express();
+
+app.get('/summoner/:summonerName', function (req, res) {
+  request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/' + req.params.summonerName + '?api_key=' + apiKey, function(error, response) {
+      if (!error && response.statusCode == 200) {
+          console.log(response.body);
+          res.send(response.body);
+      }
+  });
 });
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
