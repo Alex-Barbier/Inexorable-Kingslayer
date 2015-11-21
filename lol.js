9ef368ae9150a2c217c7d95233ba62c5d3047d5c
Alexandre Barbier <barbier.a@sfeir.com>
2015-11-21 12:27:17 +0100

    refactor(lol): use template strings for url composition

--- lol.js
+++ lol.js
@@ -1,10 +1,17 @@
 const request = require('request');
 const apiKey  = require('./api-key');
 
+const apiBasis = 'https://euw.api.pvp.net/api/lol/euw/';
+const apiSuffixes = {
+    summonerByName: 'v1.4/summoner/by-name/',
+    matchListBySummoner: 'v2.2/matchlist/by-summoner/'
+};
+
 module.exports = {
     getSummonerByName : function(summonerName, callback) {
         console.log(`Fetching summoner with summonerName : ${summonerName}`);
-        request('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/' + summonerName + '?api_key=' + apiKey, function(error, response) {
+        const url = `${apiBasis}${apiSuffixes.summonerByName}${summonerName}?api_key=${apiKey}`;
+        request(url, function(error, response) {
             if (!error && response.statusCode == 200) {
                 callback(response.body);
             }
@@ -12,7 +19,8 @@ module.exports = {
     },
     getRankedMatches  : function(summonerId, callback) {
         console.log(`Fetching ranked matches with summonerId : ${summonerId}`);
-        request('https://euw.api.pvp.net/api/lol/euw/v2.2/matchlist/by-summoner/' + summonerId + '?rankedQueues=RANKED_SOLO_5x5&seasons=PRESEASON2016&api_key=' + apiKey, function(error, response) {
+        const url = `${apiBasis}${apiSuffixes.matchListBySummoner}${summonerId}?rankedQueues=RANKED_SOLO_5x5&seasons=PRESEASON2016&api_key=${apiKey}`;
+        request(url, function(error, response) {
             if (!error && response.statusCode == 200) {
                 callback(response.body);
             }
