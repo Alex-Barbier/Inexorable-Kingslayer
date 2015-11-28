60619f851a2af358a187f2f5a235cef74a80ead3
Alexandre Barbier <a.barbier@epmistes.net>
2015-11-28 23:14:37 +0100

    feat(server): add CORS handler

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app.get('/', function(req, res, next) {
   });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(express.static('front'));
 
 app.get('/login/:summonerName', function(req, res) {
