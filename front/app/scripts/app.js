1883c5b632d0b9b570313c7ec50ccfe8be05a659
tonygoussot <tonygoussot@users.noreply.github.com>
2015-11-28 22:06:26 +0100

    feat(front): working starting front w/o router

---
+++ front/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name inexorableKingslayerApp
+ * @description
+ * # inexorableKingslayerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('inexorableKingslayerApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ front/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inexorableKingslayerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inexorableKingslayerApp
+ */
+angular.module('inexorableKingslayerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
