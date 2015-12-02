6400c23692ec06f4ff7fb8858015daaa66e5ad53
Alexandre Barbier <barbier.a@sfeir.com>
2015-12-02 17:37:36 +0100

    feat(server): add rate limiting on riot api calls

--- lol/lol.js
+++ lol/lol.js
@@ -1,6 +1,9 @@
-const request = require('request');
-const apiKey  = require('../api-key');
-const chalk   = require('chalk');
+'use strict';
+
+const request   = require('request');
+const apiKey    = require('../api-key');
+const chalk     = require('chalk');
+const rateLimit = require('function-rate-limit');
 
 const region         = 'euw';
 const apiBasis       = `https://${region}.api.pvp.net/api/lol/${region}/`;
@@ -12,52 +15,45 @@ const apiSuffixes = {
   summonerByName      : 'v1.4/summoner/by-name/',
   matchListBySummoner : 'v2.2/matchlist/by-summoner/',
   match               : 'v2.2/match/'
-}; 
+};
+
+// https://developer.riotgames.com/docs/api-keys
+// Apply first rate limitation : 10 request every 10 seconds
+let apiCall = rateLimit(10, 10 * 1000, (url, callback) => {
+  request(url, (error, response) => {
+    if (!error && response.statusCode == 200) {
+      callback(response.body);
+    }
+  });
+});
+
+// Apply second rate limitation : 500 request every 10 minutes
+apiCall = rateLimit(500, 10 * 60 * 1000, apiCall);
 
 module.exports = {
   getStaticVersion      : function(callback) {
     console.log(chalk.blue(`Fetching static api version`));
     const url = `${staticVersions}`;
-    request(url, function(error, response) {
-      if (!error && response.statusCode == 200) {
-        callback(response.body);
-      }
-    });
+    apiCall(url, callback);
   },
   getChampionsListImage : function(callback) {
     console.log(chalk.blue(`Fetching static champion list with image`));
     const url = `${apiStaticBasis}${apiSuffixes.staticChampions}?champData=image&api_key=${apiKey}`;
-    request(url, function(error, response) {
-      if (!error && response.statusCode == 200) {
-        callback(response.body);
-      }
-    });
+    apiCall(url, callback);
   },
   getSummonerByName     : function(summonerName, callback) {
     console.log(chalk.blue(`Fetching summoner with summonerName : ${summonerName}`));
     const url = `${apiBasis}${apiSuffixes.summonerByName}${summonerName}?api_key=${apiKey}`;
-    request(url, function(error, response) {
-      if (!error && response.statusCode == 200) {
-        callback(response.body);
-      }
-    });
+    apiCall(url, callback);
   },
   getRankedMatches      : function(summonerId, callback) {
     console.log(chalk.blue(`Fetching ranked matches with summonerId : ${summonerId}`));
     const url = `${apiBasis}${apiSuffixes.matchListBySummoner}${summonerId}?rankedQueues=RANKED_SOLO_5x5&seasons=SEASON2015&api_key=${apiKey}`;
-    request(url, function(error, response) {
-      if (!error && response.statusCode == 200) {
-        callback(response.body);
-      }
-    });
+    apiCall(url, callback);
   },
   getMatch: function(matchId, callback) {
-    console.log(`Fetching matches with matchId : ${matchId}`);
+    console.log(chalk.blue(`Fetching matches with matchId : ${matchId}`));
     const url = `${apiBasis}${apiSuffixes.match}${matchId}?api_key=${apiKey}`;
-    request(url, function(error, response) {
-      if (!error && response.statusCode == 200) {
-        callback(response.body);
-      }
-    });
+    apiCall(url, callback);
   }
 };
