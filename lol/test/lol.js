e0429100bc4893c1cd3d1af3b6571bd46ed6047a
Alexandre Barbier <a.barbier@epmistes.net>
2015-12-08 23:33:25 +0100

    feat(lol): add first unit test with tape

---
+++ lol/test/lol.js
@@ -0,0 +1,8 @@
+const test = require('tape');
+const lol = require('../lol');
+
+test('test api static version', function (t) {
+    lol.getStaticVersion(function(staticVersionData){
+      t.equal(JSON.parse(staticVersionData)[0], '5.25.1')
+    });
+});
