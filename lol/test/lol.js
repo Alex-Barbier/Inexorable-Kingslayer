3a55c72c966e23bf8329a369922f9d9a0bfc5ebc
Alexandre Barbier <a.barbier@epmistes.net>
2015-12-08 23:37:51 +0100

    feat(lol): test now passing, added nyan reporter

--- lol/test/lol.js
+++ lol/test/lol.js
@@ -3,6 +3,6 @@ const lol = require('../lol');
 
 test('test api static version', function (t) {
     lol.getStaticVersion(function(staticVersionData){
-      t.equal(JSON.parse(staticVersionData)[0], '5.25.1')
+      t.equal(JSON.parse(staticVersionData)[0], '5.24.1')
     });
 });
